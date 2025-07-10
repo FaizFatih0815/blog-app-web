a9b87703e6a6e910729bb9d46970da12b945b7e2
FaizFatih0815 <m.f.alfatih@gmail.com>
2025-07-10 21:37:34 +0700

    feat: add login

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,43 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
-  return (
-    <div className="bg-gray-400">
-      <nav className="container mx-auto flex items-center justify-between px-4 py-2">
-        <p>Logo</p>
+  const router = useRouter();
+  const session = useSession();
 
-        <div className="flex gap-4">
-          <p>Home</p>
-          <p>Contact</p>
+  const logout = () => {
+    signOut({ redirect: false });
+    router.push("/login");
+  };
+
+  return (
+    <div className="bg-gray-300">
+      <nav className="container mx-auto flex items-center justify-between p-4">
+        <Link href="/">
+          <p className="text-2xl font-bold">BlogHub</p>
+        </Link>
+
+        <div className="flex items-center gap-4">
+          <Link href="/">Home</Link>
+          {!session.data?.user ? (
+            <>
+              <Link href="/login">Login</Link>
+              <Link href="/register">Register</Link>
+            </>
+          ) : (
+            <>
+              <p className="font-bold capitalize">{session.data.user.name}</p>
+              <Button variant="destructive" onClick={logout}>
+                Logout
+              </Button>
+            </>
+          )}
         </div>
       </nav>
     </div>
---
+++ src/features/login/index.tsx
@@ -0,0 +1,11 @@
+import FormLogin from "./components/FormLogin";
+
+const LoginPage = () => {
+  return (
+    <main className="container mx-auto px-4">
+      <FormLogin />
+    </main>
+  );
+};
+
+export default LoginPage;
