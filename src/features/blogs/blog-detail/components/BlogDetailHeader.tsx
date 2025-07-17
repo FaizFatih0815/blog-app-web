42978e8a695dc653f43f68128b060692d70fa11c
FaizFatih0815 <139619851+FaizFatih0815@users.noreply.github.com>
2025-07-17 20:24:51 +0700

    feat: add delete blog (#6)

--- src/features/blogs/blog-detail/components/BlogDetailHeader.tsx
+++ src/features/blogs/blog-detail/components/BlogDetailHeader.tsx
@@ -2,12 +2,16 @@ import { Badge } from "@/components/ui/badge";
 import { Blog } from "@/types/blog";
 import { format } from "date-fns";
 import Image from "next/image";
+import ModalConfirmationDeleteBlog from "./ModalConfirmationDeleteBlog";
+import { useSession } from "next-auth/react";
 
 interface BlogDetailHeaderProps {
   blog: Blog;
 }
 
 const BlogDetailHeader = ({ blog }: BlogDetailHeaderProps) => {
+  const session = useSession();
+
   return (
     <section className="space-y-2">
       <Badge
@@ -19,9 +23,15 @@ const BlogDetailHeader = ({ blog }: BlogDetailHeaderProps) => {
 
       <h1 className="text-2xl font-bold md:text-3xl">{blog.title}</h1>
 
-      <p>
-        {format(blog.createdAt, "dd MMMM yyyy")} - {blog.user?.name}
-      </p>
+      <div className="flex items-center justify-between">
+        <p>
+          {format(blog.createdAt, "dd MMMM yyyy")} = {blog.user?.name}
+        </p>
+
+        {Number(session.data?.user.id) === blog.user?.id && (
+          <ModalConfirmationDeleteBlog blogId={blog.id} />
+        )}
+      </div>
 
       <div className="relative h-[250px] overflow-hidden md:h-[360px]">
         <Image
